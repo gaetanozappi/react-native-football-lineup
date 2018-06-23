d616be7e2a3c5d93b099d34e61744d2ec4ff5c20
Zappi <Zappi>
2018-06-23 16:57:08 +0200

    Init

---
+++ FootballField.js
@@ -0,0 +1,262 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Image, ImageBackground } from 'react-native';
+
+export default class FootballField extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      home: props.home,
+      away: props.away,
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ImageBackground
+          source={require('./images/footballfield.png')}
+          style={{
+            width: null,
+            height: null,
+            flex: 1,
+          }}>
+          <View style={{ backgroundColor: 'rgba(52, 52, 52, 0.45)', flex: 1 }}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'rgba(204, 70, 43, 0)',
+              }}>
+              <View
+                style={{
+                  flexDirection: 'row',
+                }}>
+                <Text
+                  style={[
+                    styles.text,
+                    {
+                      marginLeft: 20,
+                    },
+                  ]}>
+                  {this.state.home.module}
+                </Text>
+                <Text
+                  style={[
+                    styles.text,
+                    {
+                      position: 'absolute',
+                      right: 20,
+                    },
+                  ]}>
+                  {this.state.home.name}
+                </Text>
+              </View>
+
+              {this.state.home.team.map((data, i) => (
+                <View style={styles.rowHome}>
+                  {data.map((d, j) => (
+                    <View style={styles.el}>
+                      <View
+                        style={{
+                          flexDirection: 'row',
+                        }}>
+                        {this.state.home.home_team_events.map(
+                          (el, z) =>
+                            d.name == el.player &&
+                            el.type_of_event == 'yellow-card' && (
+                              <Image
+                                source={require('./imagess/card-yellow.png')}
+                                style={{
+                                  width: 12,
+                                  height: 15,
+                                  position: 'absolute',
+                                  left: -12,
+                                }}
+                              />
+                            )
+                        )}
+                        {this.state.home.home_team_events.map(
+                          (el, z) =>
+                            d.name == el.player &&
+                            el.type_of_event == 'red-card' && (
+                              <Image
+                                source={require('./images/card-red.png')}
+                                style={{
+                                  width: 12,
+                                  height: 15,
+                                  position: 'absolute',
+                                  left: -12,
+                                }}
+                              />
+                            )
+                        )}
+                        <View style={styles.playHome}>
+                          <Text style={styles.text}>{d.number}</Text>
+                        </View>
+                        {this.state.home.home_team_events.map(
+                          (el, z) =>
+                            d.name == el.player &&
+                            el.type_of_event == 'substitution-in' && (
+                              <Image
+                                source={require('./images/refresh.png')}
+                                style={{
+                                  width: 12,
+                                  height: 12,
+                                  position: 'absolute',
+                                  right: -14,
+                                }}
+                              />
+                            )
+                        )}
+                      </View>
+                      <Text style={styles.text}>{d.name}</Text>
+                    </View>
+                  ))}
+                </View>
+              ))}
+            </View>
+
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'rgba(43, 99, 204, 0)',
+                paddingTop: 55,
+              }}>
+              {this.state.away.team.reverse().map((data, i) => (
+                <View style={styles.rowAway}>
+                  {data.map((d, j) => (
+                    <View style={styles.el}>
+                      <Text style={styles.text}>{d.name}</Text>
+                      <View
+                        style={{
+                          flexDirection: 'row',
+                        }}>
+                        {this.state.away.away_team_events.map(
+                          (el, z) =>
+                            d.name == el.player &&
+                            el.type_of_event == 'yellow-card' && (
+                              <Image
+                                source={require('./images/card-yellow.png')}
+                                style={{
+                                  width: 12,
+                                  height: 15,
+                                  position: 'absolute',
+                                  left: -12,
+                                }}
+                              />
+                            )
+                        )}
+                        {this.state.away.away_team_events.map(
+                          (el, z) =>
+                            d.name == el.player &&
+                            el.type_of_event == 'red-card' && (
+                              <Image
+                                source={require('./images/card-red.png')}
+                                style={{
+                                  width: 12,
+                                  height: 15,
+                                  position: 'absolute',
+                                  left: -12,
+                                }}
+                              />
+                            )
+                        )}
+                        <View style={styles.playAway}>
+                          <Text style={styles.text}>{d.number}</Text>
+                        </View>
+                        {this.state.away.away_team_events.map(
+                          (el, z) =>
+                            d.name == el.player &&
+                            el.type_of_event == 'substitution-in' && (
+                              <Image
+                                source={require('./images/refresh.png')}
+                                style={{
+                                  width: 12,
+                                  height: 12,
+                                  position: 'absolute',
+                                  right: -14,
+                                }}
+                              />
+                            )
+                        )}
+                      </View>
+                    </View>
+                  ))}
+                </View>
+              ))}
+
+              <View
+                style={{
+                  flexDirection: 'row',
+                }}>
+                <Text
+                  style={[
+                    styles.text,
+                    {
+                      marginLeft: 20,
+                    },
+                  ]}>
+                  {this.state.away.module}
+                </Text>
+                <Text
+                  style={[
+                    styles.text,
+                    {
+                      position: 'absolute',
+                      right: 20,
+                    },
+                  ]}>
+                  {this.state.away.name}
+                </Text>
+              </View>
+            </View>
+          </View>
+        </ImageBackground>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  rowHome: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  rowAway: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  el: {
+    flexGrow: 1,
+    alignItems: 'center',
+  },
+  playHome: {
+    justifyContent: 'center',
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    borderWidth: 2,
+    borderColor: 'rgb(244, 67, 54)',
+    backgroundColor: 'rgba(244, 67, 54,0.5)',
+  },
+  playAway: {
+    justifyContent: 'center',
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    borderWidth: 2,
+    borderColor: 'rgb(3, 169, 244)',
+    backgroundColor: 'rgba(3, 169, 244,0.5)',
+  },
+  text: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: '#fff',
+    textShadowColor: 'rgba(0, 0, 0, 0.75)',
+    textShadowOffset: { width: 0, height: 1 },
+    textShadowRadius: 1,
+    fontSize: 15,
+  },
+});
--- README.md
+++ README.md
@@ -1,1 +1,212 @@
-# react-native-football-lineup
+# React Native: Football Lineup
+
+[![github home](https://img.shields.io/badge/gaetanozappi-react--native--football--lineup-blue.svg?style=flat-square)](https://github.com/gaetanozappi/react-native-football-lineup)
+[![github issues](https://img.shields.io/github/issues/gaetanozappi/react-native-football-lineup.svg?style=flat-square)](https://github.com/gaetanozappi/react-native-football-lineup/issues)
+
+![PNG](screenshot/react-native-football-lineup.png)
+
+-   [Usage](#usage)
+-   [License](#license)
+
+## Usage
+
+```javascript
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+import FootballField from './FootballField';
+
+var home = {
+  name: 'POR',
+  module: '4-4-2',
+  team: [
+    [
+      {
+        number: 1,
+        name: 'Patricio',
+      },
+    ],
+    [
+      {
+        number: 21,
+        name: 'Soares',
+      },
+      {
+        number: 3,
+        name: 'Pepe',
+      },
+      {
+        number: 6,
+        name: 'Fonte',
+      },
+      {
+        number: 5,
+        name: 'Guerriero',
+      },
+    ],
+    [
+      {
+        number: 14,
+        name: 'Calvalho',
+      },
+      {
+        number: 8,
+        name: 'Mountinho',
+      },
+      {
+        number: 11,
+        name: 'Silva',
+      },
+      {
+        number: 17,
+        name: 'Guedes',
+      },
+    ],
+    [
+      {
+        number: 16,
+        name: 'Fernandes',
+      },
+      {
+        number: 7,
+        name: 'Cristiano Ronaldo',
+      },
+    ],
+  ],
+  home_team_events: [
+    {
+      id: 203,
+      type_of_event: 'red-card',
+      player: 'Silva',
+      time: "3'",
+    },
+    {
+      id: 210,
+      type_of_event: 'yellow-card',
+      player: 'Fernandes',
+      time: "64'",
+    },
+    {
+      id: 210,
+      type_of_event: 'yellow-card',
+      player: 'Fonte',
+      time: "64'",
+    },
+    {
+      id: 206,
+      type_of_event: 'substitution-in',
+      player: 'Fonte',
+      time: "31'",
+    },
+  ],
+};
+
+var away = {
+  name: 'SPA',
+  module: '4-2-3-1',
+  team: [
+    [
+      {
+        number: 1,
+        name: 'De Gea',
+      },
+    ],
+    [
+      {
+        number: 18,
+        name: 'Alba',
+      },
+      {
+        number: 15,
+        name: 'Ramos',
+      },
+      {
+        number: 3,
+        name: 'Pique',
+      },
+      {
+        number: 4,
+        name: 'Nacho',
+      },
+    ],
+    [
+      {
+        number: 8,
+        name: 'Koke',
+      },
+      {
+        number: 5,
+        name: 'Busquets',
+      },
+    ],
+    [
+      {
+        number: 6,
+        name: 'Iniesta',
+      },
+      {
+        number: 22,
+        name: 'Isco',
+      },
+      {
+        number: 21,
+        name: 'Silva',
+      },
+    ],
+    [
+      {
+        number: 19,
+        name: 'Costa',
+      },
+    ],
+  ],
+  away_team_events: [
+    {
+      id: 210,
+      type_of_event: 'yellow-card',
+      player: 'De Gea',
+      time: "12'",
+    },
+    {
+      id: 206,
+      type_of_event: 'substitution-in',
+      player: 'Iniesta',
+      time: "31'",
+    },
+    {
+      id: 206,
+      type_of_event: 'substitution-in',
+      player: 'Costa',
+      time: "32'",
+    },
+    {
+      id: 206,
+      type_of_event: 'red-card',
+      player: 'Silva',
+      time: "31'",
+    },
+  ],
+};
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <FootballField home={home} away={away} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 35,
+    borderColor: '#fff',
+    backgroundColor: '#000',
+  },
+});
+
+```
+
+## License
+The MIT License
